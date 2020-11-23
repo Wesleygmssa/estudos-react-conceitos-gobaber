00dc3db5f64cf2d41b56d9d9a131de5fda27e77e
wesley guerra <guerra_ssa@hotmail.com>
2020-11-23 11:50:41 -0300

    renomendo rota de autenticação

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Switch } from 'react-router-dom';
-import Route from './Route'
+import Route from './RouteAuth'
 
 import SignIn from '../pages/Singin';
 import SignUp from '../pages/SingUp';
