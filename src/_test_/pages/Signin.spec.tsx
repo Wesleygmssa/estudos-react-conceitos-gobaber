5a4916017f64210f09c3c624d9e7ba771b26a845
Wesley Guerra <guerra_ssa@hotmail.com>
2021-04-13 23:35:10 -0300

    Teste SigIn criado com sucesso

--- src/_test_/pages/Signin.spec.tsx
+++ src/_test_/pages/Signin.spec.tsx
@@ -3,6 +3,9 @@ import SignIn from "../../pages/Singin";
 import React from "react";
 
 const mockedHistoryPush = jest.fn();
+const mockedSignIn = jest.fn();
+
+const mockedAddToast = jest.fn();
 
 jest.mock("react-router-dom", () => {
   return {
@@ -16,7 +19,15 @@ jest.mock("react-router-dom", () => {
 jest.mock("../../hooks/AuthContext", () => {
   return {
     useAuth: () => ({
-      signIn: jest.fn(),
+      signIn: mockedSignIn,
+    }),
+  };
+});
+
+jest.mock("../../hooks/ToastContext", () => {
+  return {
+    useToast: () => ({
+      addToast: mockedAddToast,
     }),
   };
 });
@@ -36,6 +47,48 @@ describe("SignIn Page", () => {
 
     await waitFor(() => {
       expect(mockedHistoryPush).toHaveBeenCalledWith("/dashboard");
+    });
+  });
+
+  it("Should not be able to sigin in with invalid credentials", async () => {
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
+
+    const emailField = getByPlaceholderText("E-mail");
+    const passwordField = getByPlaceholderText("Senha");
+    const buttonElement = getByText("Entrar");
+
+    fireEvent.change(emailField, { target: { value: "not-valid-email" } });
+    fireEvent.change(passwordField, { target: { value: "123456" } });
+
+    fireEvent.click(buttonElement);
+
+    await waitFor(() => {
+      expect(mockedHistoryPush).not.toHaveBeenCalledWith("/dashboard");
+    });
+  });
+
+  it("Should display an error if login error", async () => {
+    mockedSignIn.mockImplementation(() => {
+      throw new Error();
+    });
+
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
+
+    const emailField = getByPlaceholderText("E-mail");
+    const passwordField = getByPlaceholderText("Senha");
+    const buttonElement = getByText("Entrar");
+
+    fireEvent.change(emailField, { target: { value: "johndoe@example.com" } });
+    fireEvent.change(passwordField, { target: { value: "123456" } });
+
+    fireEvent.click(buttonElement);
+
+    await waitFor(() => {
+      expect(mockedAddToast).toHaveBeenCalledWith(
+        expect.objectContaining({
+          type: "error",
+        })
+      );
     });
   });
 });
